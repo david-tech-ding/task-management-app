ef7001234d2ac527bfc9b8745bb7cecdd27cc0a5
Max Tauroza <maxtauroza@gmail.com>
2023-07-18 21:24:07 +0100

    Create TaskSummary component and style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,15 @@
 import React from "react";
+import TaskSummary from "./TaskSummary";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <TaskSummary title="Your Tasks" tasks={[{ priorityLevel: "High" }]} />
+      <TaskSummary title="Tasks Assigned By You" tasks={[]} />
+      <TaskSummary title="Due Soon" tasks={[]} />
+    </div>
+  );
 };
 
 export default Dashboard;
