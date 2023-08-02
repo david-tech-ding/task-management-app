9e28ca65e20e9a6ca340476eba0b2d185afaf271
David Ding <dingtechxing16@gmail.com>
2023-08-02 23:02:20 +0100

    Testing for App and CreateAccount components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,15 @@
-// import React from "react";
-// import { render, screen, waitFor } from "@testing-library/react";
-// import axios from "axios";
-// import { MemoryRouter } from "react-router-dom";
-// import App from "./App";
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "../components/App";
 
-// jest.mock("axios");
-// axios.get.mockResolvedValue({ data: [] });
-
-// describe("App component", () => {
-//   test("renders the navbar with correct props", () => {
-//     render(
-//       <MemoryRouter>
-//         <App />
-//       </MemoryRouter>
-//     );
-//     expect(screen.getByRole("navigation")).toBeInTheDocument();
-//     expect(screen.getByRole("navigation")).toHaveClass("navbar");
-//   });
-// });
+describe("App component", () => {
+  it("renders correectly", () => {
+    const { asFragment } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
